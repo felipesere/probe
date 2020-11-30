e6f8d363f6cb9bc2331d9db16d47cc231b1e2b41
Felipe Sere <felipesere@gmail.com>
2020-11-30 09:29:24 +0100

    Allow sorting by key, time changed, and status

--- cmd/ls.go
+++ cmd/ls.go
@@ -1,18 +1,36 @@
 package cmd
 
 import (
+	"fmt"
+	"github.com/bradfitz/slice"
 	"github.com/felipesere/probe/lib"
 	"github.com/spf13/cobra"
 	"sort"
+	"time"
 )
 
 var (
+	sortingBy string
+
 	lsCmd = &cobra.Command{
 		Use:   "ls",
 		Short: "list c",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			items := db.LoadData()
-			lib.Print(items, byKey)
+			var sortingFunc func(map[int]lib.GithubData) []int
+
+			switch sortingBy {
+			case "changed":
+				sortingFunc = byLastUpdate
+			case "key":
+				sortingFunc = byKey
+			case "status":
+				sortingFunc = byStatus
+			default:
+				return fmt.Errorf("unknown sorting function: %s", sortingBy)
+			}
+
+			lib.Print(items, sortingFunc)
 
 			return nil
 		},
@@ -28,6 +46,55 @@ func byKey(data map[int]lib.GithubData) []int {
 	return keys
 }
 
+func byLastUpdate(data map[int]lib.GithubData) []int {
+	type byLastChange struct {
+		id          int
+		lastChanged time.Time
+	}
+
+	var toBeSorted []byLastChange
+	for _, item := range data {
+		toBeSorted = append(toBeSorted, byLastChange{
+			id:          item.Key,
+			lastChanged: item.LastUpdated,
+		})
+	}
+	sort.Sort(slice.SortInterface(toBeSorted[:], func(i, j int) bool {
+		return toBeSorted[i].lastChanged.After(toBeSorted[j].lastChanged)
+	}))
+
+	var keys []int
+	for _, x := range toBeSorted {
+		keys = append(keys, x.id)
+	}
+	return keys
+}
+
+func byStatus(data map[int]lib.GithubData) []int {
+	type sortByStatus struct {
+		id     int
+		status string
+	}
+
+	var toBeSorted []sortByStatus
+	for _, item := range data {
+		toBeSorted = append(toBeSorted, sortByStatus{
+			id:     item.Key,
+			status: item.Status,
+		})
+	}
+	sort.Sort(slice.SortInterface(toBeSorted[:], func(i, j int) bool {
+		return toBeSorted[i].status < toBeSorted[j].status
+	}))
+
+	var keys []int
+	for _, x := range toBeSorted {
+		keys = append(keys, x.id)
+	}
+	return keys
+}
+
 func init() {
+	lsCmd.Flags().StringVar(&sortingBy, "sort-by", "key", "choose from 'key', 'changed', or 'status' to sort the table")
 	rootCmd.AddCommand(lsCmd)
 }
--- go.mod
+++ go.mod
@@ -3,12 +3,13 @@ module github.com/felipesere/probe
 go 1.15
 
 require (
-	github.com/davecgh/go-spew v1.1.1
+	github.com/bradfitz/slice v0.0.0-20180809154707-2b758aa73013
 	github.com/dustin/go-humanize v1.0.0
 	github.com/olekukonko/tablewriter v0.0.4
 	github.com/shurcooL/githubv4 v0.0.0-20200928013246-d292edc3691b
 	github.com/shurcooL/graphql v0.0.0-20200928012149-18c5c3165e3a // indirect
 	github.com/spf13/cobra v1.1.1
 	github.com/spf13/viper v1.7.0
-	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
+	go4.org v0.0.0-20200411211856-f5505b9728dd // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 )
